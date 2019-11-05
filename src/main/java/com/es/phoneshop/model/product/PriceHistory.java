414041ddbe1c79e6d8139c575ff0ba1fa25cbbbb
Andrew <mr.yahovskiy@mail.ru>
2019-11-05 18:35:18 +0300

    Task 2.5 [selfcheck]: Price history

---
+++ src/main/java/com/es/phoneshop/model/product/PriceHistory.java
@@ -0,0 +1,29 @@
+package com.es.phoneshop.model.product;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Date;
+
+public class PriceHistory {
+    private String date;
+    private BigDecimal price;
+    private Currency currency;
+
+    public PriceHistory(String date, BigDecimal price, Currency currency) {
+        this.date = date;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+}
